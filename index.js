69e9f764964323721cf91e89d93b94162f66f745
Hakan Bilgin <hbi99@hotmail.com>
2016-06-07 02:46:41 +0200

    testing

--- index.js
+++ index.js
@@ -1,6 +1,33 @@
 
 'use strict';
 
+var fs = require('fs'),
+	chokidar = require('chokidar'),
+	through2 = require('through2'),
+	less = require('less');
+
+// fs.createReadStream('README.md')
+// 	.pipe(through2(function (chunk, enc, callback) {
+// 		console.log(chunk, enc, callback);
+// 	}));
+
+var watcher = chokidar.watch('README.md', {
+	ignored: /[\/\\]\./,
+	persistent: true
+});
+
+watcher
+	.on('add', function(path) {
+		console.log(`File ${path} has been added`)
+	})
+	.on('change', function(path) {
+		console.log(`File ${path} has been changed`)
+	})
+	.on('unlink', function(path) {
+		console.log(`File ${path} has been removed`);
+	});
+
+
 module.exports = {
 	init: function() {
 		
